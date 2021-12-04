089fd5dfba5e50bccf7d89dad00727787e47abd2
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 01:08:51 +1100

    Added Window settings structure to store data about the window settings

--- Framework/Engine/Game.cs
+++ Framework/Engine/Game.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Framework.Engine
 {
+    public struct WindowSettings
+    {
+        public int WindowWidth;
+        public int WindowHeight;
+        public int bIsFullscreen;
+        public string WindowTitle;
+    }
+
     public class Game
     {
 
+        
+
+        Game()
+        {
+            
+        }
+
+        public void Initialize(WindowSettings window)
+        {
+
+        }
     }
 }
