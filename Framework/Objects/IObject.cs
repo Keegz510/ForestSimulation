55b20a3567970cb45aa806938917da26e6d7f30d
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 10:25:30 +1100

    Added Interface to be used on all objects

---
+++ Framework/Objects/IObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public interface IObject
+    {
+        public abstract void Init();
+        public abstract void OnInit();
+        public abstract void Update();
+        public abstract void OnUpdate();
+        public abstract void Destroy();
+        public abstract void OnDestroy();
+
+    }
+}
