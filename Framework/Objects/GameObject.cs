621b355bb86a1241873937dfba99475dbfdd5972
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 20:19:05 +1100

    Made the update methods virtual

--- Framework/Objects/GameObject.cs
+++ Framework/Objects/GameObject.cs
@@ -143,7 +143,7 @@ namespace Framework
         }
 
 
-        public void Update(float deltaTime)
+        public virtual void Update(float deltaTime)
         {
             if (!bIsActive) return;
 
