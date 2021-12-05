a168dc5a160c70b6bb25557da132cf95f7599c4e
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 17:32:24 +1100

    Added resource manager with a method to load game objects and a method to generate an ID

---
+++ Framework/Engine/ResourceManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework;
+using static Raylib_cs.Raymath;
+namespace Framework.Engine
+{
+    public class ResourceManager
+    {
+        /// List of ID's been used
+        private List<string> usedIDs = new List<string>();
+        /// List of game objects in the resource manager
+        private List<IObject> gameObjects = new List<IObject>();
+        // TODO: Add Owning State
+
+        public ResourceManager()
+        {
+
+        }
+
+        public void LoadGameObject(IObject go)
+        {
+            if(!gameObjects.Contains(go))
+            {
+                gameObjects.Add(go);
+                go.SetID(GenerateID());
+            }
+        }
+
+        private string GenerateID()
+        {
+            string[] characters = { "a", "b", "c", "d", "e", "f", "g", "0", "1", "2", "3", "4", "5", "!", "@", "#", "$", "%" };
+
+            bool bIsValid = false;
+
+            do
+            {
+                var idLength = Raylib_cs.Raylib.GetRandomValue(6, 15);
+                var newID = "";
+
+                for (int i = 0; i < idLength; ++i)
+                {
+                    newID += characters[Raylib_cs.Raylib.GetRandomValue(0, characters.Length)];
+                }
+
+                if (!usedIDs.Contains(newID))
+                {
+                    bIsValid = true;
+                    usedIDs.Add(newID);
+                    return newID;
+                }
+            } while (!bIsValid);
+
+            return "";
+        }
+    }
+}
