7a2ef47d4825ed9525b6d671900087a8f065ba67
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 19:52:12 +1100

    Created an interface for tagable objects

---
+++ Framework/Objects/ITagable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public interface ITagable
+    {
+        public void AddTag(string tag);
+        public void AddTags(List<string> tags);
+        public void RemoveTag(string tag);
+        public void HasTag(string tag);
+    }
+}
