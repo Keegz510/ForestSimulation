81711565231fc95317a37394b95be5e0c2b54788
Keegz510 <s200322@students.aie.edu.au>
2021-12-05 20:48:16 +1100

    Created the initial in game state

---
+++ ForestSimulationCSharp/GameStates/InGameState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.Engine;
+using Framework;
+
+namespace ForestSim
+{
+    public class InGameState : GameState
+    {
+        public InGameState() : base()
+        { }
+
+        public override void Init()
+        {
+            base.Init();
+
+            resourceManager.LoadGameObject(new GameObject());
+        }
+    }
+}
