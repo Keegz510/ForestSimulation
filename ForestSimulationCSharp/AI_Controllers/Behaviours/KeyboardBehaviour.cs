8a89b8e3eabab4ef9c587ee111fdc1d68484108b
Keegz510 <s200322@students.aie.edu.au>
2021-12-07 15:42:53 +1100

    Adjusted Movement Speed

--- ForestSimulationCSharp/AI_Controllers/Behaviours/KeyboardBehaviour.cs
+++ ForestSimulationCSharp/AI_Controllers/Behaviours/KeyboardBehaviour.cs
@@ -11,16 +11,21 @@ namespace ForestSim
 {
     public class KeyboardBehaviour : IBehavior
     {
-        private float movementSpeed = 50.0f;
+        private float movementSpeed = 1200.0f;
 
         public void Update(Agent agent, float deltaTime)
         {
             Vector2 force = new Vector2();
 
-            if (IsKeyDown(KeyboardKey.KEY_W)) force.y = -movementSpeed;
-            if(IsKeyDown(KeyboardKey.KEY_S)) force.y = movementSpeed;
-            if (IsKeyDown(KeyboardKey.KEY_A)) force.x = -movementSpeed;
-            if(IsKeyDown(KeyboardKey.KEY_D)) force.x =movementSpeed
+            if (IsKeyDown(KeyboardKey.KEY_W)) 
+                force.y = -movementSpeed;
+            if (IsKeyDown(KeyboardKey.KEY_S)) 
+                force.y = movementSpeed;
+            if (IsKeyDown(KeyboardKey.KEY_A)) 
+                force.x = -movementSpeed;
+            if (IsKeyDown(KeyboardKey.KEY_D)) 
+                force.x = movementSpeed;
+
 
             agent.AddForce(force);
         }
