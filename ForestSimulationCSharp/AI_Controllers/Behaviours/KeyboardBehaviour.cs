c29cfca5eb01bd945c2cbaa7c2bb4763992ab9f6
Keegz510 <s200322@students.aie.edu.au>
2021-12-07 13:34:52 +1100

    Added a beviour to handle movement with keys (this will apply for the camera)

---
+++ ForestSimulationCSharp/AI_Controllers/Behaviours/KeyboardBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.Maths;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace ForestSim
+{
+    public class KeyboardBehaviour : IBehavior
+    {
+        private float movementSpeed = 50.0f;
+
+        public void Update(Agent agent, float deltaTime)
+        {
+            Vector2 force = new Vector2();
+
+            if (IsKeyDown(KeyboardKey.KEY_W)) force.y = -movementSpeed;
+            if(IsKeyDown(KeyboardKey.KEY_S)) force.y = movementSpeed;
+            if (IsKeyDown(KeyboardKey.KEY_A)) force.x = -movementSpeed;
+            if(IsKeyDown(KeyboardKey.KEY_D)) force.x =movementSpeed
+
+            agent.AddForce(force);
+        }
+    }
+}
