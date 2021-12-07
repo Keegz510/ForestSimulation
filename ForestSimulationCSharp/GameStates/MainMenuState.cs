3ada734a4d757f5a67350b1edebdbb8d06a3d8dd
Keegz510 <s200322@students.aie.edu.au>
2021-12-07 12:24:09 +1100

    Create Background image, Implemented Menu Buttons for starting a new simulation & Exitting the game.

--- ForestSimulationCSharp/GameStates/MainMenuState.cs
+++ ForestSimulationCSharp/GameStates/MainMenuState.cs
@@ -13,38 +13,88 @@ namespace ForestSim
 {
     public class MainMenuState : GameState
     {
+        Button newSimBtn = null;
+        Button ExitSimBtn = null;
+        Sprite background = null;
         public MainMenuState() : base()
         { }
 
         public override void Init()
         {
             base.Init();
+
+            background = resourceManager.LoadSprite("Images/MainMenuBackground.png");
+
+
+            CreateNewSimBtn();
+            CreateExitButton();
+        }
+
+        public void CreateNewSimBtn()
+        {
             Size buttonSize = new Size
             {
                 Width = 170,
                 Height = 50
             };
 
-            Button newSimBtn = new Button(buttonSize, LoadSim, "LoadSimBtn");
+            // Create the button and set the scale to be 2
+            Button btn = new Button(buttonSize, LoadSim, "LoadSimBtn");
+            btn.Scale = 2;
 
-            Sprite newSimSprite = resourceManager.LoadSprite("Images/Button_Background.png");
+            // Set the position of the button
+            btn.SetPosition(new Vector2(30, 70));
 
-            resourceManager.LoadGameObject(newSimSprite);
+            Font font = LoadFont("Fonts/Pergola.otf");
+            TextField btnText = new TextField("New Simulation", font, "NewSimText");
+            resourceManager.AddDrawable(btnText);
+            btnText.SetParent(btn);
+            var textPos = new Vector2
+            {
+                x = buttonSize.Width / 2,
+                y = buttonSize.Height / 2
+            };
+            btnText.SetPosition(textPos);
+            btnText.SetTint(Color.BLACK);
 
-            newSimSprite.SetParent(newSimBtn);
-
-            newSimBtn.SetPosition(new Vector2(30, 70));
-
-
-            resourceManager.LoadGameObject(newSimBtn);
-            
+            newSimBtn = btn;
         }
 
-        
+        public void CreateExitButton()
+        {
+            Size buttonSize = new Size
+            {
+                Width = 170,
+                Height = 50
+            };
+
+            // Create the button and set the scale to be 2
+            Button btn = new Button(buttonSize, LoadSim, "ExitSimBtn");
+            btn.Scale = 2;
+            // Handle loading the new game object
+            resourceManager.LoadGameObject(btn);
+
+            // Set the position of the button
+            btn.SetPosition(new Vector2(30, 150));
+
+            Font font = LoadFont("Fonts/Pergola.otf");
+            TextField btnText = new TextField("Exit Simulation", font, "ExitSimText");
+            resourceManager.AddDrawable(btnText);
+            btnText.SetParent(btn);
+            var textPos = new Vector2
+            {
+                x = buttonSize.Width / 2,
+                y = buttonSize.Height / 2
+            };
+            btnText.SetPosition(textPos);
+            btnText.SetTint(Color.BLACK);
+
+            ExitSimBtn = btn;
+        }
 
         public void LoadSim()
         {
-
+            Debug.LogMsg("Test Message");
         }
 
         public void ExitSim()
